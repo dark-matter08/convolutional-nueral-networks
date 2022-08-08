599844b5eafff295f4dd2597f26596bc2c665e23
Nde Lucien <chelucien08@gmail.com>
2022-08-08 11:54:56 +0100

    ok

--- p7.py
+++ p7.py
@@ -7,7 +7,6 @@ loss = -(math.log(softmax_output[0]) * target_output[0] +
          math.log(softmax_output[1]) * target_output[1] +
          math.log(softmax_output[2]) * target_output[2])
 
-
 print(loss)
 
 
